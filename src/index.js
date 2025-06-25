6e11e8eee04e50d9d60c9668efa0b7b2b7c9df66
Derek <19647340+ddinan@users.noreply.github.com>
2025-06-26 09:13:52 +1000

    Create basic game scene + controls

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import net from 'net'
 import Packet from './packets.js'
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // === CONFIGURATION ===
 const host = '127.0.0.1'
@@ -79,3 +85,11 @@ client.on('close', () => {
 client.on('error', (err) => {
   console.error('Error:', err)
 })
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.use('../build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('../jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
+
+app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
